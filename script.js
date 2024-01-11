ed6958e005e84df342dac9eeb33a3e4dcb555359
PLD908 <oluyededivine@gmail.com>
2024-01-11 12:35:31 +0100

    Added some code

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const questionBank = [
     {
     question: "what is the capital of France?",
     options: ["Paris", "London", "Berlin", "Mardrid"],
-    answer: "paris",
+    answer: "Paris",
 },
     {
     question: "what is the largest planet in our solar system?",
@@ -16,3 +16,44 @@ const questionBank = [
     answer: "France",
 },
 ]
+const quizContainer = document.getElementById('quiz');
+const resultContainer = document.getElementById('result');
+const submitButton = document.getElementById('submit');
+const retryButton = document.getElementById('retry');
+const showAnswerButton = document.getElementById('showAnswer');
+
+//keeping track of the question, score and answer
+let currentQuestion = 0;
+let score = 0;
+let incorrectAnswer = [];
+
+function displayQuestion() {
+    const questionData = questionBank[currentQuestion];
+    const questionElement = document.createElement("div");
+    questionElement.className = "question";
+    questionElement.innerHTML = questionBank.question;
+
+    const optionsElement = document.createElement("div");
+    optionsElement.className = "options";
+    
+    const shuffledOptions = [...questionData.options];
+    shuffleArray(shuffledOptions);
+    for (let i = 0; i < shuffledOptions.length; i++) {
+        const option = document.createElement("label");
+        option.className = "option";
+
+        //radio for each option
+        const radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = "quiz";
+        radio.value = shuffledOptions[i];
+        const optionText = document.createTextNode(shuffledOptions[i]);
+        
+        option.appendChild(radio);
+        option.appendChild(optionText);
+        optionsElement.appendChild(option);
+    }
+    quizContainer.innerHTML = "";
+    quizContainer.appendChild(questionElement);
+    quizContainer.appendChild(optionsElement);
+}
