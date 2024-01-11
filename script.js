87e6ce6aa1d867487d9824aeeaf96c8473506d46
PLD908 <oluyededivine@gmail.com>
2024-01-11 11:15:45 +0100

    Quiz App

---
+++ script.js
@@ -0,0 +1,18 @@
+//let construct or question bank
+const questionBank = [
+    {
+    question: "what is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Mardrid"],
+    answer: "paris",
+},
+    {
+    question: "what is the largest planet in our solar system?",
+    options: ["Mars", "Saturn", "Jupiter", "Neptune"],
+    answer: "Jupiter",
+},
+    {
+    question: "which country won the FIFA World Cup in 2018?",
+    options: ["Argentina", "Brazil", "Germany", "France"],
+    answer: "France",
+},
+]
